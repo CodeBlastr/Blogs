84833bbce7271292942a1a15de1bfe51d88ac99b
Joel Byrnes <joel@razorit.com>
2014-06-04 17:59:22 -0400

    formatting

--- Controller/BlogPostsController.php
+++ Controller/BlogPostsController.php
@@ -1,9 +1,10 @@
 <?php
 
-App::uses('BlogsAppController','Blogs.Controller');
+App::uses('BlogsAppController', 'Blogs.Controller');
+
 /**
- *@property BlogPost BlogPost
- *@property Blog Blog
+ * @property BlogPost BlogPost
+ * @property Blog Blog
  */
 class AppBlogPostsController extends BlogsAppController {
 
@@ -13,7 +14,6 @@ class AppBlogPostsController extends BlogsAppController {
  * Uses
  *
  */
-
 	public $uses = array('Blogs.BlogPost');
 
 /**
@@ -22,13 +22,13 @@ class AppBlogPostsController extends BlogsAppController {
  */
 	public function __construct($request = null, $response = null) {
 		parent::__construct($request, $response);
-		if (in_array('Recaptcha', CakePlugin::loaded())) {
+		if (CakePlugin::loaded('Recaptcha')) {
 			$this->components[] = 'Recaptcha.Recaptcha';
 		}
-		if (in_array('Comments', CakePlugin::loaded())) {
+		if (CakePlugin::loaded('Comments')) {
 			$this->components['Comments.Comments'] = array('userModelClass' => 'User');
 		}
-		if (in_array('Categories', CakePlugin::loaded())) {
+		if (CakePlugin::loaded('Categories')) {
 			$this->uses[] = 'Categories.Category';
 		}
 	}
@@ -47,27 +47,27 @@ class AppBlogPostsController extends BlogsAppController {
  *
  * @todo 		Need to find a better way more reusable way to use recaptcha
  */
-	public function view($id=null) {
+	public function view($id = null) {
 		// temporary recaptcha placement
 		if (!empty($this->request->data)) {
 			if ($this->Recaptcha->verify()) {
-		        // do something, save you data, login, whatever
-		    } else {
-		        // display the raw API error
-		        $this->Session->setFlash($this->Recaptcha->error);
+				// do something, save you data, login, whatever
+			} else {
+				// display the raw API error
+				$this->Session->setFlash($this->Recaptcha->error);
 				$this->redirect($this->referer());
-		    }
+			}
 		}
-		$blogPost = $this->BlogPost->find('first',array(
+		$blogPost = $this->BlogPost->find('first', array(
 			'conditions' => array(
 				'BlogPost.id' => $id,
-				),
+			),
 			'contain' => array(
 				'Author',
 				'Category.name',
 				'Category.id'
-				),
-			));
+			),
+		));
 
 		$this->paginate = array(
 			'limit' => 5,
@@ -76,14 +76,14 @@ class AppBlogPostsController extends BlogsAppController {
 		);
 
 		$this->paginate = array('Comment' => array(
-			'order' => array('Comment.created' => 'desc'),
-			'recursive' => 0,
-			'limit' => 5,
-			'conditions'=>array('model' => 'Blogs.BlogPost')
+				'order' => array('Comment.created' => 'desc'),
+				'recursive' => 0,
+				'limit' => 5,
+				'conditions' => array('model' => 'Blogs.BlogPost')
 		));
 
 		$this->set('categories', $blogPost['Category']);
-		$this->set('blogPost',$blogPost);
+		$this->set('blogPost', $blogPost);
 		$this->set('title_for_layout', __('%s | %s', $blogPost['BlogPost']['title'], __SYSTEM_SITE_NAME));
 		$this->set('page_title_for_layout', $blogPost['BlogPost']['title']);
 	}
@@ -93,29 +93,27 @@ class AppBlogPostsController extends BlogsAppController {
  */
 	public function add($blogId = null) {
 
-		if(strtolower($blogId) == 'my'){
+		if (strtolower($blogId) == 'my') {
 			$myBlogId = $this->BlogPost->Blog->findBlogIdByOwnerId($this->userId);
-			$this->redirect(array('action'=>'add',$myBlogId));
+			$this->redirect(array('action' => 'add', $myBlogId));
 			return;
 		}
 		$this->BlogPost->Blog->id = $blogId;
 		if (!$this->BlogPost->Blog->exists()) {
 			throw new NotFoundException(__('Invalid blog.'));
 		}
-		if(!empty($this->request->data)) {
+		if (!empty($this->request->data)) {
 			try {
 				unset($this->request->data['BlogPost']['id']);
 				$this->BlogPost->save($this->request->data);
 				$this->Session->setFlash('Blog Post Saved');
 				$this->redirect(array('action' => 'view', $this->BlogPost->id));
 			} catch (Exception $e) {
-
 				$this->Session->setFlash($e->getMessage());
-
 			}
 		}
 		$authors = $this->BlogPost->Author->find('list');
-		if (in_array('Categories', CakePlugin::loaded())) {
+		if (CakePlugin::loaded('Categories')) {
 			$categories = $this->BlogPost->Category->find('list', array('conditions' => array('Category.model' => 'BlogPost')));
 		} else {
 			$categories = null;
@@ -124,15 +122,13 @@ class AppBlogPostsController extends BlogsAppController {
 		$blog = $this->BlogPost->Blog->find('first', array('conditions' => array('Blog.id' => $blogId)));
 		$page_title_for_layout = __('Add Blog Post to %s', $blog['Blog']['title']);
 
-		$userGroups = Set::combine($this->BlogPost->Author->UserGroup->UsersUserGroup->getUserGroups(),
-			'{n}.UserGroup.id','{n}.UserGroup.title');
+		$userGroups = Set::combine($this->BlogPost->Author->UserGroup->UsersUserGroup->getUserGroups(), '{n}.UserGroup.id', '{n}.UserGroup.title');
 
-		$this->set(compact('authors', 'blogId', 'categories', 'statuses', 'page_title_for_layout','userGroups'));
-
+		$this->set(compact('authors', 'blogId', 'categories', 'statuses', 'page_title_for_layout', 'userGroups'));
 	}
 
-	public function getMetaDescripton($url=''){
-		$metaData = get_meta_tags(((strpos($url,'http://') === 0 || (strpos($url,'https://') ===0 ) ?  '' : 'http://')) . $url);
+	public function getMetaDescripton($url = '') {
+		$metaData = get_meta_tags(((strpos($url, 'http://') === 0 || (strpos($url, 'https://') === 0 ) ? '' : 'http://')) . $url);
 		$desc = isset($metaData['description']) ? $metaData['description'] : '';
 		print_r($desc);
 		$this->autoRender = false;
@@ -147,7 +143,7 @@ class AppBlogPostsController extends BlogsAppController {
 			throw new NotFoundException(__('Invalid post.'));
 		}
 
-		if(!empty($this->request->data)) {
+		if (!empty($this->request->data)) {
 			try {
 				$this->BlogPost->save($this->request->data);
 				$this->Session->setFlash('Blog Post Saved');
@@ -156,25 +152,25 @@ class AppBlogPostsController extends BlogsAppController {
 				$this->Session->setFlash($e->getMessage());
 			}
 		}
-		
+
 		$contain[] = 'Author';
-		if (in_array('Categories', CakePlugin::loaded())) {
+		if (CakePlugin::loaded('Categories')) {
 			$categories = $this->BlogPost->Category->generateTreeList(array('Category.model' => 'BlogPost'));
 			$this->set(compact('categories'));
 			$contain[] = 'Category';
 		}
-		if (in_array('Tags', CakePlugin::loaded())) {
+		if (CakePlugin::loaded('Tags')) {
 			$tags = $this->BlogPost->Tag->Tagged->find('cloud', array('conditions' => array('Tagged.foreign_key' => $id)));
 			$this->request->data['BlogPost']['tags'] = implode(', ', Set::extract('/Tag/name', $tags));
 		}
 
-		$blogPost = $this->BlogPost->find('first',array(
+		$blogPost = $this->BlogPost->find('first', array(
 			'conditions' => array(
 				'BlogPost.id' => $id
-				),
+			),
 			'contain' => $contain,
-			));
-		
+		));
+
 		$this->request->data = $blogPost;
 		$this->set('selectedCategories', Set::extract('/id', $this->request->data['Category']));
 		$authors = $this->BlogPost->Author->find('list');
@@ -187,16 +183,16 @@ class AppBlogPostsController extends BlogsAppController {
  * Latest method (should be removed in favor of the helper / element combo)
  */
 	public function latest() {
-		if(isset($this->request->params['named']['blog_id']) && isset($this->request->params['named']['limit'])) {
-			  $options = array(
-			  	'conditions' => array(
+		if (isset($this->request->params['named']['blog_id']) && isset($this->request->params['named']['limit'])) {
+			$options = array(
+				'conditions' => array(
 					'BlogPost.blog_id' => $this->request->params['named']['blog_id'],
 					'BlogPost.published <' => date('Y-m-d h:i:s')
 				),
 				'order' => 'published DESC',
 				'limit' => $this->request->params['named']['limit']
-			  );
-			  return $this->BlogPost->find('all', $options);
+			);
+			return $this->BlogPost->find('all', $options);
 		}
 	}
 
@@ -222,5 +218,9 @@ class AppBlogPostsController extends BlogsAppController {
 }
 
 if (!isset($refuseInit)) {
-	class BlogPostsController extends AppBlogPostsController {}
+
+	class BlogPostsController extends AppBlogPostsController {
+
+	}
+
 }
